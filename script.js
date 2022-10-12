77cdc57084b82fbdff57852505997b809c146c00
DELTAMIKE7 <dhaneshhero7@gmail.com>
2022-10-13 00:38:35 +0530

    second commit

--- script.js
+++ script.js
@@ -9,6 +9,9 @@
 
 //     document.querySelector(".slide img:nth-child(2)").style.clipPath= `circle(0% at ${elem.clientX-dim.left}px ${elem.clientY-dim.top}px)`
 // })
+
+
+
 var nh1= document.querySelector("#nav>h1"),
 nopen= document.querySelector(".navopen");
 
@@ -19,6 +22,82 @@ nh1.addEventListener("click",function(){
 
 })
 
-// gsap.to(".navopen",{
-//     height: 35
-// })
+
+
+function slide() {
+    document.querySelectorAll(".slide").forEach(function (slide) {
+        slide.addEventListener("mousemove", function (dets) {
+            var dim = slide.getBoundingClientRect();
+            this.children[1].style.clipPath = `circle(15% at ${dets.clientX - dim.left}px  ${dets.clientY - dim.top}px)`;
+        })
+    })
+}
+document.querySelectorAll(".slide").forEach(function (slide) {
+    slide.addEventListener("mouseout", function (dets) {
+        var dim = slide.getBoundingClientRect();
+        this.children[1].style.clipPath = `circle(0% at ${dets.clientX - dim.left}px  ${dets.clientY - dim.top}px)`;
+    })
+})
+slide()
+
+function skew_maker() {
+    var dim1 = document.querySelector(".slide").getBoundingClientRect();
+    var prev = dim1.left;
+
+
+    document.querySelector(".project").addEventListener("scroll", function (elmm) {
+        var dim2 = document.querySelector(".slide").getBoundingClientRect();
+        var diff = prev - dim2.left;
+        document.querySelectorAll(".slide").forEach(function (elm) {
+            elm.style.transform = `skew(${diff * .07}deg)`; 
+        })
+        prev = dim2.left;
+        
+    })
+}
+skew_maker()
+
+
+
+var nopen = document.querySelector(".navopen");
+var cls = document.querySelector("#cls");
+var flag= 1;
+document.querySelector("#dix").addEventListener("mouseover",function(){ 
+    if(flag===1){
+        nopen.style.display="flex"
+        lolo();
+        flag=0;
+    }
+    // nopen.addEventListener("mouseover",function(){
+    //     if(flag===1){
+    //         nopen.style.display="flex"
+    //         lolo();
+    //     }
+    // })
+
+
+}
+)
+cls.addEventListener("click",function(){
+    nopen.style.display="none"
+    flag=1;
+
+})
+
+
+function lolo(){
+gsap.to(".navopen",{
+    width: "28vw",
+    padding: "14vh 2vw",
+    height: "fit-content",
+    opacity: 1
+})
+}
+function tolo(){
+    gsap.from(".navopen",{
+        width: "0vw",
+        padding: "0vh 0",
+        height: "0vh",
+        opacity: 0,
+    })
+    }
